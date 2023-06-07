8e6b07e0802c8925e9d4a309025573c24db0ebda
Craig-UK <craigukdev@gmail.com>
2023-06-07 02:38:49 +0100

    CHORE: Update home/index page.

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
-import Image from 'next/image'
+import React from 'react'
 
-export default function Home() {
+const Home = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       Welcome to my Portfolio.
     </main>
   )
 }
+
+export default Home
