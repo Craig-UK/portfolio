3b23fa480a1e46c1614c7473d566b8ee7ced3e45
Craig-UK <craigukdev@gmail.com>
2023-06-13 22:39:11 +0100

    FEAT: Create the structure of the 'Other Pages Card' component that can be used on each of the pages.

--- components/Cards/OtherPagesCard.jsx
+++ components/Cards/OtherPagesCard.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
 
-const OtherPagesCard = () => {
+const OtherPagesCard = ({ pageTitle, cardTitle, cardText, button }) => {
   return (
-    <div>OtherPagesCard</div>
-  )
-}
+    <div className="other_pages_card">
+      <div className="font-satoshi">
+        <h4>{pageTitle}</h4>
+      </div>
+      <div className="font-satoshi font-semibold text-2xl">
+        <h1>{cardTitle}</h1>
+      </div>
+      {/* ABOUT PARAGRAPH */}
+      <div>
+        <p>{cardText}</p>
+      </div>
+      {/* BUTTON */}
+      <div>
+        {button &&
+          button.map((button) => (
+            <button key={button.id} className="other_pages_cta_btn">
+              <Link href={button.href}>{button.name}</Link>
+            </button>
+          ))}
+      </div>
+    </div>
+  );
+};
 
-export default OtherPagesCard
+export default OtherPagesCard;
