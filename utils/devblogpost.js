0c623e2a4b5bc12f6abcd8df59404afcca50bf06
Craig-UK <craigukdev@gmail.com>
2023-06-23 23:43:22 +0100

    FIX: Lint issues.

--- utils/devblogpost.js
+++ utils/devblogpost.js
@@ -1,24 +1,24 @@
-import fs from "fs";
-import path from "path";
-import matter from "gray-matter";
-import { remark } from "remark";
-import html from "remark-html";
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import { remark } from 'remark'
+import html from 'remark-html'
 
-const postsDirectory = path.join(process.cwd(), "dev_blog_posts");
+const postsDirectory = path.join(process.cwd(), 'dev_blog_posts')
 
-export function getSortedPostsData() {
+export function getSortedPostsData () {
   // Get file names under /posts
-  const fileNames = fs.readdirSync(postsDirectory);
+  const fileNames = fs.readdirSync(postsDirectory)
   const allPostsData = fileNames.map((fileName) => {
-    // Remove ".md" from file name to get id
-    const id = fileName.replace(/\.md$/, "");
+    // Remove '.md' from file name to get id
+    const id = fileName.replace(/\.md$/, '')
 
     // Read markdown file as string
-    const fullPath = path.join(postsDirectory, fileName);
-    const fileContents = fs.readFileSync(fullPath, "utf8");
+    const fullPath = path.join(postsDirectory, fileName)
+    const fileContents = fs.readFileSync(fullPath, 'utf8')
 
     // Use gray-matter to parse the post metadata section
-    const matterResult = matter(fileContents);
+    const matterResult = matter(fileContents)
 
     const blogPost = {
       id,
@@ -26,28 +26,28 @@ export function getSortedPostsData() {
       date: matterResult.data.date,
       author: matterResult.data.author,
       image: matterResult.data.image,
-      summary: matterResult.data.summary,
-    };
+      summary: matterResult.data.summary
+    }
 
     // Combine the data with the id
-    return blogPost;
-  });
+    return blogPost
+  })
   // Sort posts by date
-  return allPostsData.sort((a, b) => (a.date < b.date ? 1 : -1));
+  return allPostsData.sort((a, b) => (a.date < b.date ? 1 : -1))
 }
 
-export async function getPostData(id) {
-  const fullPath = path.join(postsDirectory, `${id}.md`);
-  const fileContents = fs.readFileSync(fullPath, "utf8");
+export async function getPostData (id) {
+  const fullPath = path.join(postsDirectory, `${id}.md`)
+  const fileContents = fs.readFileSync(fullPath, 'utf8')
 
   // Use gray-matter to parse the post metadata section
-  const matterResult = matter(fileContents);
+  const matterResult = matter(fileContents)
 
   const processedContent = await remark()
     .use(html)
-    .process(matterResult.content);
+    .process(matterResult.content)
 
-  const contentHtml = processedContent.toString();
+  const contentHtml = processedContent.toString()
 
   const blogPostWithHTML = {
     id,
@@ -56,9 +56,9 @@ export async function getPostData(id) {
     author: matterResult.data.author,
     image: matterResult.data.image,
     summary: matterResult.data.summary,
-    contentHtml,
-  };
+    contentHtml
+  }
 
   // Combine the data with the id
-  return blogPostWithHTML;
+  return blogPostWithHTML
 }
