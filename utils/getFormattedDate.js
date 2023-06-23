95f78a0c191f00adc056ec8e2032a2818f5dfe42
Craig-UK <craigukdev@gmail.com>
2023-06-24 00:07:20 +0100

    FIX: Lint issues.

--- utils/getFormattedDate.js
+++ utils/getFormattedDate.js
@@ -1,4 +1,4 @@
-export default function getFormattedDate(dateString) {
+export default function getFormattedDate (dateString) {
   const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
   const date = new Date(dateString)
 
