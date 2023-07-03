4d201e7b2b36c570c07471d9efaa7a2cbebc1043
Craig-UK <craigukdev@gmail.com>
2023-07-03 20:11:09 +0100

    FEAT: Create the 'Single Desktop Application Project' page and fetch the Desktop Application Projects from the database.

---
+++ components/Single-Project-Page/SingleDesktopProject.jsx
@@ -0,0 +1,40 @@
+import Image from "next/image"
+import Link from "next/link"
+import { Carousel } from "react-responsive-carousel"
+import "react-responsive-carousel/lib/styles/carousel.min.css"
+
+const SingleDesktopProject = ({ title, github, deployed, images, desc, limits }) => {
+  return (
+    <div className='font-satoshi'>
+       <h1 className='text-4xl font-bold'>{title}</h1>
+       <div className='flex justify-start gap-5'>
+        <Link href={github}>{github}</Link>
+        {deployed === "Not Deployed" ? (
+          <p>Not Deployed</p>
+        ): (
+          <Link href={deployed}>{deployed}</Link>
+        )}
+       </div>
+       <div className='flex justify-center mt-5'>
+          <Carousel>
+          {images?.map(img => (
+          <div key={img}>
+              <Image 
+                src={img}
+                alt='Image on Single Web Project Page'
+                width={220}
+                height={220}
+              />
+          </div>
+        ))}
+        </Carousel>
+       </div>
+       <div className='mt-5'>
+        <p>{desc}</p>
+        <p>{limits}</p> 
+       </div>
+    </div>
+  )
+}
+
+export default SingleDesktopProject
