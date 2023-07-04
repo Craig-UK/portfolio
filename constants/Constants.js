d1f3692404b99a3a7f22cf2b82cf7e7aadf90438
Craig-UK <craigukdev@gmail.com>
2023-07-05 00:28:43 +0100

    FIX: Lint issues.

--- constants/Constants.js
+++ constants/Constants.js
@@ -1,19 +1,19 @@
 export const footerLinks = [
-    {
-        title: "About",
-        links: [
-            { title: "About", url: "/about" },
-            { title: "Projects", url: "/projects" },
-            { title: "Dev Blog", url: "/devblog" },
-            { title: "Travel Blog", url: "/travelblog" }
-        ]
-    },
-    {
-        title: "Socials",
-        links: [
-            { title: "Twitter", url: "https://twitter.com/CraigDevUK" },
-            { title: "Instagram", url: "https://instagram.com/CraigDevUK" },
-            { title: "GitHub", url: "https://github.com/Craig-UK" }
-        ]
-    }
+  {
+    title: 'About',
+    links: [
+      { title: 'About', url: '/about' },
+      { title: 'Projects', url: '/projects' },
+      { title: 'Dev Blog', url: '/devblog' },
+      { title: 'Travel Blog', url: '/travelblog' }
+    ]
+  },
+  {
+    title: 'Socials',
+    links: [
+      { title: 'Twitter', url: 'https://twitter.com/CraigDevUK' },
+      { title: 'Instagram', url: 'https://instagram.com/CraigDevUK' },
+      { title: 'GitHub', url: 'https://github.com/Craig-UK' }
+    ]
+  }
 ]
