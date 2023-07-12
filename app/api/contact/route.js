008921cc1c6c6f1e0853e98eb309726edb163f92
Craig-UK <craigukdev@gmail.com>
2023-07-12 03:07:38 +0100

    FIX: Lint issues.

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -18,10 +18,10 @@ export async function POST (request) {
 
     const accessToken = await new Promise((resolve, reject) => {
       oauth2Client.getAccessToken((err, token) => {
-        if(err) {
-          reject('Failed to create access token');
+        if (err) {
+          reject(new Error('Failed to create access token'))
         }
-          resolve(token);
+        resolve(token)
       })
     })
 
