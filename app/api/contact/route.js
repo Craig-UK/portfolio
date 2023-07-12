b6ba5aab7e0e145dbaef391c40ea16b9505465d8
Craig-UK <craigukdev@gmail.com>
2023-07-12 03:03:14 +0100

    FIX: Lint issues.

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -1,6 +1,6 @@
-import nodemailer from "nodemailer"
-import { google } from "googleapis"
-import { NextResponse } from "next/server"
+import nodemailer from 'nodemailer'
+import { google } from 'googleapis'
+import { NextResponse } from 'next/server'
 
 export async function POST (request) {
   const { name, email, phone, linkedin, message } = await request.json()
@@ -8,25 +8,24 @@ export async function POST (request) {
 
   const createTransporter = async () => {
     const oauth2Client = new OAuth2(
-        process.env.CLIENT_ID,
-        process.env.CLIENT_SECRET,
-        process.env.REQUEST_URI
-    );
-    
+      process.env.CLIENT_ID,
+      process.env.CLIENT_SECRET,
+      process.env.REQUEST_URI
+    )
     oauth2Client.setCredentials({
-        refresh_token: process.env.REFRESH_TOKEN
-    });
-
-    const accessToken = await new Promise((resolve, reject) => {
-        oauth2Client.getAccessToken((err, token) => {
-            if(err) {
-                reject('Failed to create access token');
-            }
-            resolve(token);
-        })
+      refresh_token: process.env.REFRESH_TOKEN
     })
 
-   const transporter = nodemailer.createTransport({
+    const accessToken = await new Promise((resolve, reject) => {
+      oauth2Client.getAccessToken((err, token) => {
+        if(err) {
+          reject('Failed to create access token');
+        }
+          resolve(token);
+      })
+    })
+
+    const transporter = nodemailer.createTransport({
       host: 'smtp.gmail.com',
       port: 465,
       secure: true,
