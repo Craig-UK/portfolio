c19a10dc7406a1933907e97f98eef65bf648eb94
Craig-UK <craigukdev@gmail.com>
2023-07-16 20:49:25 +0100

    FEAT: Add a link to the footer to allow users to email me by clicking the link.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -8,7 +8,10 @@ const Footer = () => {
     <div className='flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10'>
       <div className='flex flex-col justify-start items-start gap-6'>
         <p className='text-base text-gray-700 font-bold'>
-          Craig Cimie - Developer
+          Craig Climie - Developer
+        </p>
+        <p className="font-semibold text-gray-700">
+          <a href='mailto:contact@justcraigdev.com' className="hover:underline">Contact Me through email by clicking here</a>
         </p>
       </div>
 
