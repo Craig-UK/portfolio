187678b166cf996fd96b9f42f04c190f8da233b3
Craig-UK <craigukdev@gmail.com>
2023-08-19 00:54:07 +0100

    CHORE: Move the 'Subject' combobox into its own 'SubjectList' component.

--- components/Contact-Form/ContactForm.jsx
+++ components/Contact-Form/ContactForm.jsx
@@ -1,9 +1,8 @@
 "use client";
 
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
-import { Fragment, useState } from "react";
-import { Listbox, Transition } from '@headlessui/react'
-import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
+import { useState } from "react";
+import SubjectList from "./SubjectList";
 
 const subjects = [
   {
@@ -15,10 +14,6 @@ const subjects = [
     name: 'Projects',
   }
 ]
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
 
 const ContactForm = () => {
   const initValues = {
@@ -225,68 +220,7 @@ const ContactForm = () => {
       <label htmlFor="subject" className="px-2 pt-2">
         Subject<span className="text-red-600">*</span>
       </label>
-      <Listbox value={selected} onChange={setSelected}>
-      {({ open }) => (
-        <div className="w-fit">
-          <div className="relative mt-2">
-            <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
-              <span className="flex items-center">
-                <span className="ml-3 block truncate">{selected.name}</span>
-              </span>
-              <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
-                <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-              </span>
-            </Listbox.Button>
-
-            <Transition
-              show={open}
-              as={Fragment}
-              leave="transition ease-in duration-100"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              <Listbox.Options className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                {subjects.map((subject) => (
-                  <Listbox.Option
-                    key={subject.id}
-                    className={({ active }) =>
-                      classNames(
-                        active ? 'bg-indigo-600 text-white' : 'text-gray-900',
-                        'relative cursor-default select-none py-2 pl-3 pr-9'
-                      )
-                    }
-                    value={subject}
-                  >
-                    {({ selected, active }) => (
-                      <>
-                        <div className="flex items-center">
-                          <span
-                            className={classNames(selected ? 'font-semibold' : 'font-normal', 'ml-3 block truncate')}
-                          >
-                            {subject.name}
-                          </span>
-                        </div>
-
-                        {selected ? (
-                          <span
-                            className={classNames(
-                              active ? 'text-white' : 'text-indigo-600',
-                              'absolute inset-y-0 right-0 flex items-center pr-4'
-                            )}
-                          >
-                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                          </span>
-                        ) : null}
-                      </>
-                    )}
-                  </Listbox.Option>
-                ))}
-              </Listbox.Options>
-            </Transition>
-          </div>
-        </div>
-      )}
-    </Listbox>
+      <SubjectList value={selected} onChange={setSelected} subjects={subjects} />
       <label htmlFor="message" className="px-2 pt-2">
         Message<span className="text-red-600">*</span>
       </label>
