221a06765d8c2af3b359f52e5111007667577615
Craig-UK <craigukdev@gmail.com>
2023-08-19 01:40:36 +0100

    FEAT: Add a banner to the top of the website/underneath the navigation bar mentioning that this website is a work in progress.

---
+++ components/Banners/WorkInProgressBanner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WorkInProgressBanner = () => {
+  return (
+    <div className='flex w-full justify-center mb-16 bg-red-600 text-white'>
+        This website is a work in progress. Please report any issues or bugs you find to contact@justcraigdev.com.
+    </div>
+  )
+}
+
+export default WorkInProgressBanner
