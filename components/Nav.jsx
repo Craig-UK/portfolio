da13d0691ae82199be6a5fae9456795579895a29
Craig-UK <craigukdev@gmail.com>
2023-08-21 00:59:58 +0100

    FEAT: Improve the UI of the Navigation Bar and also make the Navigation Bar more mobile responsive.

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,98 +2,94 @@
 
 import Link from "next/link";
 import React, { useState } from "react";
-import { Bars3Icon } from "@heroicons/react/24/outline";
 import WorkInProgressBanner from "./Banners/WorkInProgressBanner";
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
 
 const Nav = () => {
-  const [toggleDropdown, setToggleDropdown] = useState(false);
+  const navigation = [
+    { name: 'Home', href: '/', current: false },
+    { name: 'Projects', href: '/projects', current: false },
+    { name: 'About', href: '/about', current: false },
+    { name: 'Dev Blog', href: '/devblog', current: false },
+    { name: 'Travel Blog', href: '/travelblog', current: false },
+    { name: 'Contact', href: '/contact', current: false },
+  ]
 
   return (
     <>
-      <nav className="flex-between w-full pt-3">
-        <Link href="/" className="flex gap-2 flex-center">
-          <p className="logo_text">Craig Climie - Developer</p>
-        </Link>
-
-        {/* Desktop Navigation */}
-        <div className="sm:flex gap-12 font-bold hidden">
-          <button className="nav_btn_link">
-            <Link href="/">Home</Link>
-          </button>
-          <button className="nav_btn_link">
-            <Link href="/projects">Projects</Link>
-          </button>
-          <button className="nav_btn_link">
-            <Link href="/about">About</Link>
-          </button>
-          <button className="nav_btn_link">
-            <Link href="/devblog">Dev Blog</Link>
-          </button>
-          <button className="nav_btn_link">
-            <Link href="/travelblog">Travel Blog</Link>
-          </button>
-          <button className="nav_btn_link">
-            <Link href="/contact">Contact</Link>
-          </button>
-        </div>
-
-        {/* Mobile Navigation */}
-        <div className="sm:hidden flex relative">
-          <div className="flex">
-            <Bars3Icon
-              className="h-10 w-10"
-              aria-hidden="true"
-              onClick={() => setToggleDropdown((prev) => !prev)}
-            />
-            {toggleDropdown && (
-              <div className="dropdown">
-                <Link
-                  href="/"
-                  className="dropdown_link"
-                  onClick={() => setToggleDropdown(false)}
-                >
-                  Home
-                </Link>
-                <Link
-                  href="/projects"
-                  className="dropdown_link"
-                  onClick={() => setToggleDropdown(false)}
-                >
-                  Projects
-                </Link>
-                <Link
-                  href="/about"
-                  className="dropdown_link"
-                  onClick={() => setToggleDropdown(false)}
-                >
-                  About
-                </Link>
-                <Link
-                  href="/devblog"
-                  className="dropdown_link"
-                  onClick={() => setToggleDropdown(false)}
-                >
-                  Dev Blog
-                </Link>
-                <Link
-                  href="/travelblog"
-                  className="dropdown_link"
-                  onClick={() => setToggleDropdown(false)}
-                >
-                  Travel Blog
-                </Link>
-                <Link
-                  href="/contact"
-                  className="dropdown_link"
-                  onClick={() => setToggleDropdown(false)}
-                >
-                  Contact
-                </Link>
+      <div className="min-h-full">
+        <Disclosure as="nav" className="bg-gray-800">
+          {({ open }) => (
+            <>
+              <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <div className="flex h-16 items-center justify-between">
+                  <div className="flex items-center">
+                    <div className="flex-shrink-0 text-white">
+                      Craig Climie - Developer
+                    </div>
+                    <div className="hidden md:block">
+                      <div className="ml-10 flex justify-end space-x-4">
+                        {navigation.map((item) => (
+                          <a
+                            key={item.name}
+                            href={item.href}
+                            className={classNames(
+                              item.current
+                                ? 'bg-gray-900 text-white'
+                                : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                              'rounded-md px-3 py-2 text-sm font-medium'
+                            )}
+                            aria-current={item.current ? 'page' : undefined}
+                          >
+                            {item.name}
+                          </a>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="-mr-2 flex md:hidden">
+                    {/* Mobile menu button */}
+                    <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                      <span className="absolute -inset-0.5" />
+                      <span className="sr-only">Open main menu</span>
+                      {open ? (
+                        <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                      ) : (
+                        <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                      )}
+                    </Disclosure.Button>
+                  </div>
+                </div>
               </div>
-            )}
-          </div>
-        </div>
-      </nav>
+
+              <Disclosure.Panel className="md:hidden">
+                <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
+                  {navigation.map((item) => (
+                    <Disclosure.Button
+                      key={item.name}
+                      as="a"
+                      href={item.href}
+                      className={classNames(
+                        item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                        'block rounded-md px-3 py-2 text-base font-medium'
+                      )}
+                      aria-current={item.current ? 'page' : undefined}
+                    >
+                      {item.name}
+                    </Disclosure.Button>
+                  ))}
+                </div>
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+      </div>
       <WorkInProgressBanner />
     </>
   );
