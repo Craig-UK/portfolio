2acdcebe867a945eb5a759eed60765fc6e3d42c5
Craig-UK <craigukdev@gmail.com>
2023-11-23 21:03:06 +0000

    FIX: Console error on '/projects/applications/(category)' pages. Category meaning Web Application Projects page, Mobile Application Projects page or Desktop Application Projects page.

--- components/Badges/FeaturedBadge.jsx
+++ components/Badges/FeaturedBadge.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const FeaturedBadge = () => {
   return (
-    <span class="inline-flex items-center bg-purple-200 rounded-full py-3 px-3 sm:py-0 my-1 mx-4 sm:mx-0 font-medium text-purple-700 ring-1 ring-inset ring-purple-700/10">Featured</span>
+    <span className="inline-flex items-center bg-purple-200 rounded-full py-3 px-3 sm:py-0 my-1 mx-4 sm:mx-0 font-medium text-purple-700 ring-1 ring-inset ring-purple-700/10">Featured</span>
   );
 };
 
