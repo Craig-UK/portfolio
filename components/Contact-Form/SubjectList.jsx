155afedd398278d58d8efd73887e9f67e2614ece
Craig-UK <craigukdev@gmail.com>
2023-12-08 20:54:30 +0000

    FEAT: Make the subject list on the contact form look better on smaller devices.

--- components/Contact-Form/SubjectList.jsx
+++ components/Contact-Form/SubjectList.jsx
@@ -10,7 +10,7 @@ const SubjectList = ({ value, onChange, subjects }) => {
   return (
     <Listbox value={value} onChange={onChange}>
       {({ open }) => (
-        <div className="sm:w-2/5 w-2/5">
+        <div className="sm:w-2/5 w-full">
           <div className="relative mt-2">
             <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
               <span className="flex items-center">
