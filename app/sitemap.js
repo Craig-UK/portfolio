2046d6e91e0c93a7821aecc3d47f96e4f7ba123f
Craig-UK <craigukdev@gmail.com>
2024-02-09 19:03:34 +0000

    FEAT: Add a sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,59 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.justcraigdev.com",
+      lastModified: new Date(),
+      priority: 1,
+    },
+    {
+      url: "https://www.justcraigdev.com/about",
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: "https://www.justcraigdev.com/projects",
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: "https://www.justcraigdev.com/devblog",
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: "https://www.justcraigdev.com/travelblog",
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: "https://www.justcraigdev.com/contact",
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: "https://www.justcraigdev.com/privacy-policy",
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: "https://www.justcraigdev.com/cookie-policy",
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: "https://www.justcraigdev.com/projects/applications/webapps",
+      lastModified: new Date(),
+      priority: 0.64,
+    },
+    {
+      url: "https://www.justcraigdev.com/projects/applications/mobileapps",
+      lastModified: new Date(),
+      priority: 0.64,
+    },
+    {
+      url: "https://www.justcraigdev.com/projects/applications/desktopapps",
+      lastModified: new Date(),
+      priority: 0.64,
+    },
+  ];
+}
