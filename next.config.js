5410cf9334db56834f27d5c726372b04a3095780
Craig-UK <craigukdev@gmail.com>
2024-02-24 17:38:00 +0000

    FEAT: Update social media links and add redirects
    Updated social media links on the footer. Added links to my facebook, linkedin and threads to the footer. Added redirects to my survey. Also added redirects to my social media: discord, github, twitter, instagram, linkedin, facebook and threads.

--- next.config.js
+++ next.config.js
@@ -6,6 +6,58 @@ const nextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com']
   },
+  async redirects() {
+    return [
+      {
+        source: '/survey',
+        destination: 'https://forms.gle/uGGKHmFGc6VsggLD9',
+        permanent: true,
+        basePath: false
+      },
+      {
+        source: '/discord',
+        destination: 'https://discord.gg/TnnsgFJ8gn',
+        permanent: true,
+        basePath: false
+      },
+      {
+        source: '/github',
+        destination: 'https://github.com/Craig-UK',
+        permanent: true,
+        basePath: false
+      },
+      {
+        source: '/twitter',
+        destination: 'https://twitter.com/CraigDevUK',
+        permanent: true,
+        basePath: false
+      },
+      {
+        source: '/instagram',
+        destination: 'https://instagram.com/CraigDevUK',
+        permanent: true,
+        basePath: false
+      },
+      {
+        source: '/linkedin',
+        destination: 'https://www.linkedin.com/company/just-craig-development-ltd',
+        permanent: true,
+        basePath: false
+      },
+      {
+        source: '/facebook',
+        destination: 'https://www.facebook.com/people/craigdevuk/61550988152517/',
+        permanent: true,
+        basePath: false
+      },
+      {
+        source: '/threads',
+        destination: 'https://www.threads.net/@craigdevuk',
+        permanent: true,
+        basePath: false
+      }
+    ]
+  },
   webpack (config) {
     config.experiments = {
       ...config.experiments,
